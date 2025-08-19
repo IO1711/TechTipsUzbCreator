9d0e92cb7898b6a08eeaf7a8c6971ffccb6c611f
IO1711 <52698195+IO1711@users.noreply.github.com>
2025-08-19 07:33:01 +0500

    after login home page updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -20,7 +20,9 @@ const Home = (props) => {
                 
             </div>}
             {props.authToken!==null && <div className="home-text">
-                    
+                <h1>Bilimingiz bilan bo'lishing</h1>
+                <p style={{fontSize:"19px"}}>Yuqoridagi dasturlardan birini tanlang, yangi mavzu yarating va qo'llanma yaratishni boshlang.</p>
+                <p>Sizga kerakli dastur ro'yxatda yo'qmi, bizga xabar yuboring. 5 daqiqa ichida javob oling.</p>
             </div>}
             
             <img src="homePagePicUz.png"></img>
