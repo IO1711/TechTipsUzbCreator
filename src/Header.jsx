42c5572f01ad00971933a0cdc37a92041e0a9da4
IO1711 <52698195+IO1711@users.noreply.github.com>
2025-08-21 17:00:56 +0500

    logo added to the header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,13 @@
+import {Link} from "react-router-dom";
+
 const Header = () => {
     return <>
-    
-    <div className='header'>Xush kelibsiz</div>
-    
+    <div className="header">
+      <Link to={"/"}><img src="./panda_E_white_transparent_trimmed.png"/></Link>
+    </div>
   </>
 }
 
 export default Header;
+
+//<img src="./full_logo_white.png"/>
